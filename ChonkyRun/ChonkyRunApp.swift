1e947c2f5752f28360d8df83cd0b8254e16cf7be
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-28 22:15:38 +0530

    Initial commit

---
+++ ChonkyRun/ChonkyRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChonkyRunApp.swift
+//  ChonkyRun
+//
+//  Created by Rudrank Riyam on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ChonkyRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
